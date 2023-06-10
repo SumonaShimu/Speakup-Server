6374c299859d2cf890ada64ddb5d7f3e8f29d26e
Sumona Akter Shimu <sumona59shimu@gmail.com>
2023-06-10 20:29:11 +0600

    users api added

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-// const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY)
 const port = process.env.PORT || 5000;
@@ -29,7 +28,7 @@ async function run() {
     const usersCollection = client.db("languageDb").collection("users");
     const paymentCollection = client.db("languageDb").collection("payments");
 
-    //payment intent----------------------------------------------
+    // create payment intent----------------------------------------------
     app.post('/create-payment-intent', async (req, res) => {
       const { price } = req.body;
       const amount = parseInt(price * 100);
@@ -44,14 +43,57 @@ async function run() {
       })
     })
 
+    // users related apis-------------------------------------------------
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await usersCollection.findOne(query);
+
+      if (existingUser) return res.send({ message: 'user already exists' })
+
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.get('/users/admin/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email }
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === 'admin' }
+      res.send(result);
+    })
+
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+
+    })
+
     // payment related api--------------------------------------------------
+    app.get('/payments', async (req, res) => {
+      const result = await paymentCollection.find().toArray();
+      res.send(result);
+    });
+
     app.post('/payments', async (req, res) => {
       const payment = req.body;
       const result = await paymentCollection.insertOne(payment);
-
       res.send({ result });
     })
-
 
 
     // Send a ping to confirm a successful connection
